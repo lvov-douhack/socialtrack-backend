71f93149c076aac49d7a6e68189c9d1af8ee1d51
Mikhail Bortnyk <vessimir@gmail.com>
2012-08-25 16:39:26 +0300

    Added devise and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 SocialtrackBackend::Application.routes.draw do
+  devise_for :users
+
   root to: 'index#home'
+
+  namespace :api do
+    mount Devise::Oauth2Providable::Engine => '/oauth2'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120825131526) do
+ActiveRecord::Schema.define(:version => 20120825133630) do
 
   create_table "oauth2_access_tokens", :force => true do |t|
     t.integer  "user_id"
@@ -67,5 +67,23 @@ ActiveRecord::Schema.define(:version => 20120825131526) do
   add_index "oauth2_refresh_tokens", ["expires_at"], :name => "index_oauth2_refresh_tokens_on_expires_at"
   add_index "oauth2_refresh_tokens", ["token"], :name => "index_oauth2_refresh_tokens_on_token", :unique => true
   add_index "oauth2_refresh_tokens", ["user_id"], :name => "index_oauth2_refresh_tokens_on_user_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
