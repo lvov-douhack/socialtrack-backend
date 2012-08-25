c86eea90ca14da0b9b53cdfbeeb8d0a415ed5087
Mikhail Bortnyk <vessimir@gmail.com>
2012-08-25 15:30:32 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialtrackBackend::Application.config.secret_token = 'bf64ee1decf720641bb75fed5ef84c689c372f2d11c5f179da8691d3aeb97c8756b2db95f236f2125003c6de443a43d87759ff035683796fdf0fb5f6de517574'
