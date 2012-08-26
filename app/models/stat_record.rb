8355c718d1623b33760b9a70db99d165889b2ebe
Mikhail Bortnyk <vessimir@gmail.com>
2012-08-26 07:10:30 +0300

    Refactored stat_records for the greatest justice

--- app/models/stat_record.rb
+++ app/models/stat_record.rb
@@ -11,16 +11,23 @@
 
 class StatRecord < ActiveRecord::Base
 
-#  def for_time time
-#    case time
-#    when :hour
-#      period = 1.hour.ago..Time.now
-#    when :month
-#      period = 1.month.ago..Time.now
-#    else
-#      period = 1.day.ago..Time.now
-#    end
-    #StatRecord.where(
-#  end
+  def self.for_time time
+    case time
+    when :hour
+      period   = 1.hour.ago..Time.now
+      period_s = 1.hour.seconds
+    when :month
+      period   = 1.month.ago..Time.now
+      period_s = 1.month.seconds
+    else
+      period   = 1.day.ago..Time.now
+      period_s = 1.day.seconds
+    end
+    StatRecord.where(created_at: period).group(:networkname).select([
+      :networkname,
+      "SUM(seconds) AS seconds",
+      "SUM(seconds) / #{period_s}.0 * 100 AS percent",
+    ])
+  end
 
 end
